06fca86a316722e3c2f00bef68f387a346c6a215
ubermachine <shivamkatoch@outlook.com>
2020-07-26 00:32:39 +0530

    configured mongoose and served static template

--- server/express.js
+++ server/express.js
@@ -4,13 +4,18 @@ import cookieParser from "cookie-parser";
 import helmet from "helmet";
 import cors from "cors";
 import compress from "compression";
+import Template from "./../template";
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser);
+app.use(cookieParser());
 app.use(compress());
 app.use(helmet());
 app.use(cors());
+app.get("/", (req, res) => {
+  res.status(200).send(Template());
+});
 
 export default app;
--- webpack.config.client.production.js
+++ webpack.config.client.production.js
@@ -3,17 +3,14 @@ const webpack = require('webpack')
 const CURRENT_WORKING_DIR = process.cwd()
 
 const config = {
-    name: "browser",
-    mode: "development",
-    devtool: 'eval-source-map',
+    mode: "production",
     entry: [
-        'webpack-hot-middleware/client?reload=true',
         path.join(CURRENT_WORKING_DIR, 'client/main.js')
     ],
     output: {
         path: path.join(CURRENT_WORKING_DIR , '/dist'),
         filename: 'bundle.js',
-        publicPath: '/dist/'
+        publicPath: "/dist/"
     },
     module: {
         rules: [
@@ -29,15 +26,6 @@ const config = {
                 use: 'file-loader'
             }
         ]
-    },  
-    plugins: [
-          new webpack.HotModuleReplacementPlugin(),
-          new webpack.NoEmitOnErrorsPlugin()
-    ],
-    resolve: {
-        alias: {
-          'react-dom': '@hot-loader/react-dom'
-        }
     }
 }
 
